2465c4421ad1728baa813a8d1b619b21d07a2231
rookielz <1046972310@qq.com>
2016-12-23 10:15:50 +0800

    Rookiez
    重新提交滑动

---
+++ TinyShop/Main/Cells/DrawerCell.m
@@ -0,0 +1,24 @@
+//
+//  DrawerCell.m
+//  TinyShop
+//
+//  Created by rimi on 2016/12/22.
+//  Copyright © 2016年 CXD. All rights reserved.
+//
+
+#import "DrawerCell.h"
+
+@implementation DrawerCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
